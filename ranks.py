d250f76ae44f60344b80ab96f61a2300639d38a1
OxyTom <oxytom8@gmail.com>
2021-05-30 19:02:30 +0200

    added Elite ranks

--- ranks.py
+++ ranks.py
@@ -1,3 +1,5 @@
+# Values are guessed for above Elite (Elite 1, 2, etc.)
+# TODO: Check if thoses values are correct https://elite-dangerous.fandom.com/wiki/Ranks
 merchantRanks = [
     ("Penniless", 0),
     ("M. Pennilesss", 5000),
@@ -7,7 +9,12 @@ merchantRanks = [
     ("Broker", 30000000),
     ("Entrepreneur", 140000000),
     ("Tycoon", 390000000),
-    ("Elite", 1050000000)
+    ("Elite", 1050000000),
+    ("Elite 1", 2000000000),
+    ("Elite 2", 2950000000),
+    ("Elite 3", 3900000000),
+    ("Elite 4", 485000000),
+    ("Elite 5", 580000000)
 ]
 explorerRanks = [
     ("Aimless", 0),
@@ -18,7 +25,12 @@ explorerRanks = [
     ("Pathfinder", 10000000),
     ("Ranger", 35000000),
     ("Pioneer", 116000000),
-    ("Elite", 320000000)
+    ("Elite", 320000000),
+    ("Elite 1", 524000000),
+    ("Elite 2", 728000000),
+    ("Elite 3", 932000000),
+    ("Elite 4", 1136000000),
+    ("Elite 5", 1340000000)
 ]
 combatRanks = [
     "Harmless",
@@ -29,7 +41,12 @@ combatRanks = [
     "Master",
     "Dangerous",
     "Deadly",
-    "Elite"
+    "Elite",
+    "Elite 1",
+    "Elite 2",
+    "Elite 3",
+    "Elite 4",
+    "Elite 5"
 ]
 empireRanks = [
     "None",
